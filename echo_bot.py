c7d6173ef9c6d816208ed27cfdceb3e9d15bde1b
Unknown <mgijon94@gmail.com>
2017-10-14 04:21:44 +0200

    Commented file

--- echo_bot.py
+++ echo_bot.py
@@ -16,20 +16,29 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands = ['help'])
 def random_name(message):
-    bot.reply_to(message, 'Cammon, you only need to work harder men!')
+    bot.reply_to(message, 'Call 091 for a propper assistance...!')
 
 
 ## START
 
 @bot.message_handler(commands = ['start'])
 def send_welcome(message):
-    bot.reply_to(message, 'Welcome, instrucciones de uso a añadir aquí')
+    '''
+    :param message:
+    :return:
+    '''
+    bot.reply_to(message, 'Welcome to the bot :)')
 
 
 ## ROT 13
 
 @bot.message_handler(commands = ['rot13'])
 def root(message):
+    '''
+    Encrypt/Decryrt ROT13 Algorithm.
+    :param message: /rot13 message
+    :return: message encrypt/decrypt
+    '''
     message.text = message.text[len('rot13') + 1 : ]
     message.text = algorithmROT13(message.text)
     bot.reply_to(message, message.text)
@@ -39,9 +48,9 @@ def root(message):
 @bot.message_handler(commands = ['cesare'])
 def cesar(message):
     '''
-    Cesar para encriptar. formato cesar 00 mensaje
-    :param message:
-    :return:
+    Encrypt Cesar.
+    :param message: /cesare 00 message
+    :return: encrypt message
     '''
     message.text = message.text[len('cesare') + 1: ] # con esto pillo hasta
     key =int( message.text[1:3])                        # obtengo la clave
@@ -54,9 +63,9 @@ def cesar(message):
 @bot.message_handler(commands = ['cesard'])
 def cesar(message):
     '''
-    Cesar para desencryptar. formato cesar 00 mensaje
-    :param message:
-    :return:
+    Desencrypt Cesar.
+    :param message: /cesard 00 message
+    :return: decrypt message
     '''
     message.text = message.text[len('cesard') + 1: ] # con esto pillo hasta
     key = - int( message.text[1:3])                        # obtengo la clave
